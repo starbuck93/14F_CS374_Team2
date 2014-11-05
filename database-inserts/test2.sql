32c41c34eff216a53797bc45b500c1a13ee5378d
Ethan Waddle <eaw09c@acu.edu>
2014-11-05 14:58:50 -0600

    miscellaneous text

--- database-inserts/test2.sql
+++ database-inserts/test2.sql
@@ -1,6 +1,10 @@
 -- test.sql: initial build script for 'test' database
 
 -- test.student
+
+---note that Ethan's server username is ethan   all lowercase.  Password is Waddle.
+
+
 DROP TABLE IF EXISTS test.student;
 CREATE TABLE test.student(id INT PRIMARY KEY,fname VARCHAR(20),lname VARCHAR(20), major VARCHAR(20));
 INSERT INTO test.student (id,fname,lname, major) VALUES
@@ -19,4 +23,16 @@ INSERT INTO test.class (id, prof1name, profLname, days, course, coursenumber, se
 	(2, "John", "Smith", "TR", CS, 101, 2),
 	(3, "John", "Doe", "TR", CS, 101, 3),
 	(4, "John", "Doe", "MWF", CS, 101, 4),
-	
+
+
+
+--test.class
+DROP TABLE IF EXISTS test.class;
+CREATE TABLE test.class(id INT PRIMARY KEY, prof1name VARCHAR(20), profLname VARCHAR(20), days VARCHAR(3), course VARCHAR(5), coursenumber INT, section INT);
+INSERT INTO test.class (id, prof1name, profLname, days, course, coursenumber, section) VALUES
+	(1, "John", "Smith", "MWF", CS, 101, 1),
+	(2, "John", "Smith", "TR", CS, 101, 2),
+	(3, "John", "Doe", "TR", CS, 101, 3),
+	(4, "John", "Doe", "MWF", CS, 101, 4),
+
+
