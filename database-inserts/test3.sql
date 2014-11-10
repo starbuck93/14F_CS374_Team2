c25d499102f23be2a0fba8d982e741fd19a9513b
Ethan Waddle <eaw09c@acu.edu>
2014-11-10 14:01:46 -0600

    inserted tentative test data

--- database-inserts/test3.sql
+++ database-inserts/test3.sql
@@ -4,16 +4,32 @@
 
 --Foreign keys and other relationships have not been started.
 
+--Joins, etc. will be implemented after tentative tables have been created.
 
 /*
 
 This is a more realistic set of tables than the first tests.  
 
-Tables are created according to the Relationships Beta diagram in the Design folder.
+Tables are created according to schema.txt in the Design folder, NOT the Beta Entity Relationship Diagram.  */
 
+/*
 
+Intersection Data:
+------------------
+student-section
+office-hours-assignment
+
+STUDENT has many SECTIONs                   SECTION has many STUDENTs
+PROFESSOR has many SECTIONs                 SECTION has a PROFESSOR
+COURSE has many SECTIONs                    SECTION has a COURSE
+SECTION has a ROOM                          ROOM has many SECTIONs
+BUILDING has many ROOMs                     ROOM has a BUILDING
+PROFESSOR has many OFFICE-HOURs             OFFICE-HOUR has a ROFESSOR
 
 */
+
+
+
 
 DROP TABLE IF EXISTS test.student;
 CREATE TABLE test.student(id INT PRIMARY KEY,fname VARCHAR(30), mi VARCHAR(1),lname VARCHAR(30), classification INT, hours INT);
@@ -41,10 +57,10 @@ INSERT INTO test.student (id,fname, mi, lname, classification, hours) VALUES
 	(4,"Ethan", "A",  "Waddle","CS", 4, 0);
 
 
-DROP TABLE IF EXISTS test.dow;
-CREATE TABLE test.dow(id INT PRIMARY KEY, days VARCHAR(7));
+DROP TABLE IF EXISTS test.DOW;
+CREATE TABLE test.DOW(id INT PRIMARY KEY, days VARCHAR(7));
 
-/*creates a table based on the entity Dow:
+/*creates a table based on the entity Days Of the Week (DOW):
 id(key)
 days   --can be "", "NMTWRFS", etc.  For Sections, it will be either "MWF" or "TR".  If there is a match between a character in the days string for one and the days string for another, they are on the same day.
 */
@@ -105,7 +121,9 @@ first-name (first_name)
 middle-initial (middle_inital)
 last-name (last_name)
 */
-
+insert into test.professor (id, first_name, middle_initial, last_name) values
+(1, "Reeves", "A", "Brent")
+(2, "Homer", "D", "John");
 
 /*
 According to the Schema, COURSE is not a "course" as in subject, like CS101, but an instantiation of that subject with an instructor, e.g. CS101-Taught-By-[professor ID]
@@ -122,6 +140,12 @@ sectionID
 --a student takes multiple courses and thus multiple sections, so the same studentID will appear more than once.  Therefore, it can't be a primary key.
 */
 
+insert into test.student-section (studentID, sectionID) values
+(1, 1)
+(2, 1)
+(3, 1)
+(4, 1)
+(1, 2);
 
 drop table if exists test.section;
 create table test.section(sectionID INT PRIMARY KEY, crn INT, section_number INT, time INT, duration INT, DOW VARCHAR(7), room_id INT);
@@ -135,6 +159,10 @@ duration
 Days Of Week on which the section meets
 room ID (room_ID)
 */
+insert into test.section (sectionID, crn, section_number, time, duration, DOW, room_ID) values
+
+(1, 1, 1, 1000, 80, "TR", 1)
+(2, 1, 2, 1300, 50, "MWF", 2);
 
 drop table if exists test.course;
 create table test.course(crn INT PRIMARY KEY, department VARCHAR(20), course_number INT, professor_id INT, name VARCHAR(30) );
@@ -148,3 +176,36 @@ professor_id (ID of the instructor, from entity Professor
 course name (name)
 
 */
+
+insert into test.course (crn, department, course_number, professor_id, name) values
+(1, "Computer Science", 101, 1, "CS101")
+(2, "Information Technologies",101, 2, "IT101") 
+drop table if exists test.Office-Hours;
+create table test.Office-Hours (id INT PRIMARY KEY, duration INT, DOW VARCHAR(3), room_id INT);
+/*
+office-hours
+------------
+id (key)
+time                    --start time
+duration                --integer minutes
+dow                     --string: "M" "MWF" "MW" "TR"
+room-id                 --(maps to building)
+
+
+*/
+
+drop table if exists test.Office-Hours-Assignment;
+create table test.Office-Hours-Assignment (professor-id INT, office-hourse-id INT primary key);
+/*
+office-hours-assignment            --intersection data for office-hours and professor
+-----------------------
+professor-id                            
+office-hours-id (key)     -- this MUST be sole primary key so that only one professor may be assigned to an office-hours entity
+
+
+*/
+
+
+
+
+
