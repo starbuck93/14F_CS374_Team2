3203f25789d8f3039a5de8b3278d17cfddbc00da
Ethan Waddle <eaw09c@acu.edu>
2014-11-15 12:09:04 -0600

    added a single table for Semester code.  Semester code only uses digits, with no characters that are not 0-9, so it will be represented as an integer instead of a string to make sorting easier.

---
+++ database-inserts/testdb/semester_code.sql
@@ -0,0 +1,5 @@
+create table test.semester-code (crn INT PRIMARY KEY, semester_code VARCHAR(6));
+
+insert into table test.semester-code (crn, semester_code) values 
+	(15000, 201510)
+	(15001, 201510);
